332777abd5ca64648be7e7bf5cf1b21076742b34
Bert Ciccone <21688853+bertciccone@users.noreply.github.com>
2018-03-10 09:32:20 -0800

    feat: detection from simulator images; detected coords not correct

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,34 +1,13 @@
 from styx_msgs.msg import TrafficLight
 
-# CarND Object Detection Lab
-
 import tensorflow as tf
 import numpy as np
-import matplotlib
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageColor
-import time
-from scipy.stats import norm
+import cv2
 
-#%matplotlib inline
-plt.style.use('ggplot')
+DEBUG_LEVEL = 2  # 0 no Messages, 1 Important Stuff, 2 Everything
 
-# Exercise 1 - Implement Separable Depthwise Convolution
+# Reference: Object Detection Lab Code
 
-def vanilla_conv_block(x, kernel_size, output_channels):
-    """
-    Vanilla Conv -> Batch Norm -> ReLU
-    """
-    x = tf.layers.conv2d(
-        x, output_channels, kernel_size, (2, 2), padding='SAME')
-    x = tf.layers.batch_normalization(x)
-    return tf.nn.relu(x)
-
-# TODO: implement MobileNet conv block
 def mobilenet_conv_block(x, kernel_size, output_channels):
     """
     Depthwise Conv -> Batch Norm -> ReLU -> Pointwise Conv -> Batch Norm -> ReLU
@@ -42,57 +21,11 @@ def mobilenet_conv_block(x, kernel_size, output_channels):
     x = tf.layers.batch_normalization(x)
     x = tf.nn.relu(x)
 
-
     # pointwise conv
     x = tf.layers.conv2d(x, output_channels, (1, 1), padding='SAME')
     x = tf.layers.batch_normalization(x)
 
     return tf.nn.relu(x)
-
-# constants but you can change them so I guess they're not so constant :)
-INPUT_CHANNELS = 32
-OUTPUT_CHANNELS = 512
-KERNEL_SIZE = 3
-IMG_HEIGHT = 256
-IMG_WIDTH = 256
-
-with tf.Session(graph=tf.Graph()) as sess:
-    # input
-    x = tf.constant(np.random.randn(1, IMG_HEIGHT, IMG_WIDTH, INPUT_CHANNELS), dtype=tf.float32)
-
-    with tf.variable_scope('mobile'):
-        mobilenet_conv = mobilenet_conv_block(x, KERNEL_SIZE, OUTPUT_CHANNELS)
-
-    mobile_params = [
-        (v.name, np.prod(v.get_shape().as_list()))
-        for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'mobile')
-    ]
-
-    print("MOBILENET CONV BLOCK")
-    total_mobile_params = sum([p[1] for p in mobile_params])
-    for p in mobile_params:
-        print("Variable {0}: number of params = {1}".format(p[0], p[1]))
-    print("Total number of params =", total_mobile_params)
-    print()
-
-# Object Detection Inference
-
-# Frozen inference graph files. NOTE: change the path to where you saved the models.
-#SSD_GRAPH_FILE = '/Users/Bert/Projects/Udacity/CARND/term3/quizzes/18-scene-object-detection-lab/ModelZoo/ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb'
-SSD_GRAPH_FILE = './ssd_frozen_inference_graph.pb'
-#RFCN_GRAPH_FILE = './rfcn_frozen_inference_graph.pb'
-#FASTER_RCNN_GRAPH_FILE = './faster_rcnn_frozen_inference_graph.pb'
-
-print("Number of colors/classes, one color per class")
-
-# Colors (one for each class)
-cmap = ImageColor.colormap
-print("Number of colors =", len(cmap))
-COLOR_LIST = sorted([c for c in cmap.keys()])
-
-#
-# Utility funcs
-#
 
 def filter_boxes(min_score, boxes, scores, classes):
     """Return boxes with a confidence >= `min_score`"""
@@ -122,15 +55,6 @@ def to_image_coords(boxes, height, width):
 
     return box_coords
 
-def draw_boxes(image, boxes, classes, thickness=4):
-    """Draw bounding boxes on the image"""
-    draw = ImageDraw.Draw(image)
-    for i in range(len(boxes)):
-        bot, left, top, right = boxes[i, ...]
-        class_id = int(classes[i])
-        color = COLOR_LIST[class_id]
-        draw.line([(left, top), (left, bot), (right, bot), (right, top), (left, top)], width=thickness, fill=color)
-
 def load_graph(graph_file):
     """Loads a frozen inference graph"""
     graph = tf.Graph()
@@ -142,85 +66,70 @@ def load_graph(graph_file):
             tf.import_graph_def(od_graph_def, name='')
     return graph
 
-detection_graph = load_graph(SSD_GRAPH_FILE)
-#detection_graph = load_graph(RFCN_GRAPH_FILE)
-#detection_graph = load_graph(FASTER_RCNN_GRAPH_FILE)
-
-# The input placeholder for the image.
-# `get_tensor_by_name` returns the Tensor with the associated name in the Graph.
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-# Each box represents a part of the image where a particular object was detected.
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-# Each score represent how level of confidence for each of the objects.
-# Score is shown on the result image, together with the class label.
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-
-# The classification of the object (integer id).
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-
-# Load a sample image.
-image = Image.open('assets/sample1.jpg')
-image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
-
-with tf.Session(graph=detection_graph) as sess:
-    # Actual detection.
-    (boxes, scores, classes) = sess.run([detection_boxes, detection_scores, detection_classes],
-                                        feed_dict={image_tensor: image_np})
-
-    # Remove unnecessary dimensions
-    boxes = np.squeeze(boxes)
-    scores = np.squeeze(scores)
-    classes = np.squeeze(classes)
-
-    confidence_cutoff = 0.8
-    # Filter boxes with a confidence score less than `confidence_cutoff`
-    boxes, scores, classes = filter_boxes(confidence_cutoff, boxes, scores, classes)
-
-    # The current box coordinates are normalized to a range between 0 and 1.
-    # This converts the coordinates actual location on the image.
-    width, height = image.size
-    box_coords = to_image_coords(boxes, height, width)
-
-    # Each class with be represented by a differently colored box
-    draw_boxes(image, box_coords, classes)
-
-    plt.figure(figsize=(12, 8))
-    plt.imshow(image)
-    #plt.savefig('assets/dog_boxes.jpg')
-    plt.savefig('assets/sample1_boxes.jpg')
-
-# Timing Detection
-
-def time_detection(sess, img_height, img_width, runs=10):
-    image_tensor = sess.graph.get_tensor_by_name('image_tensor:0')
-    detection_boxes = sess.graph.get_tensor_by_name('detection_boxes:0')
-    detection_scores = sess.graph.get_tensor_by_name('detection_scores:0')
-    detection_classes = sess.graph.get_tensor_by_name('detection_classes:0')
-
-    # warmup
-    gen_image = np.uint8(np.random.randn(1, img_height, img_width, 3))
-    sess.run([detection_boxes, detection_scores, detection_classes], feed_dict={image_tensor: gen_image})
-
-    times = np.zeros(runs)
-    for i in range(runs):
-        t0 = time.time()
-        sess.run([detection_boxes, detection_scores, detection_classes], feed_dict={image_tensor: image_np})
-        t1 = time.time()
-        times[i] = (t1 - t0) * 1000
-    return times
-
-with tf.Session(graph=detection_graph) as sess:
-    times = time_detection(sess, 600, 1000, runs=10)
-
-print("Detection Times")
-print(times)
-
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        SSD_GRAPH_FILE = './ssd_frozen_inference_graph.pb'
+        self.detection_graph = load_graph(SSD_GRAPH_FILE)
+
+        # The input placeholder for the image.
+        # `get_tensor_by_name` returns the Tensor with the associated name in the Graph.
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+
+        # The classification of the object (integer id).
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+
+        self.light_debug_index = 0
+
+    def get_traffic_light_image(self, image):
+        """Detect a traffic light in the image and return it in a new cropped image
+
+        Args:
+            image (cv::Mat): image containing the traffic light
+
+        Returns:
+            imaged cropped to a box around the traffic light
+
+        """
+        light_image = None
+
+        image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
+
+        with tf.Session(graph=self.detection_graph) as sess:
+            # Actual detection.
+            (boxes, scores, classes) = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes],
+            feed_dict={self.image_tensor: image_np})
+
+            # Remove unnecessary dimensions
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes)
+
+            confidence_cutoff = 0.7
+            # Filter boxes with a confidence score less than `confidence_cutoff`
+            boxes, scores, classes = filter_boxes(confidence_cutoff, boxes, scores, classes)
+
+            # The current box coordinates are normalized to a range between 0 and 1.
+            # This converts the coordinates actual location on the image.
+            width, height, channels = image.shape
+            box_coords = to_image_coords(boxes, height, width)
+
+            for i in range(len(boxes)):
+                bot, left, top, right = box_coords[i, ...]
+                class_id = int(classes[i])
+                if class_id == 10:
+                    light_image = image[int(bot):int(top), int(left):int(right)]
+                    if DEBUG_LEVEL >= 2:
+                        print("TL Classifier light box", int(bot), int(top), int(left), int(right))
+                        cv2.imwrite("assets/light_image" + str(self.light_debug_index) + ".jpg", light_image)
+                        self.light_debug_index += 1
+        return light_image
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -233,4 +142,7 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+
+        self.get_traffic_light_image(image)
+
         return TrafficLight.UNKNOWN

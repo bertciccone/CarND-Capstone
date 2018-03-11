a9e9f32ff528c3e6a19dc51c658a12360627fc91
Bert Ciccone <21688853+bertciccone@users.noreply.github.com>
2018-03-11 08:38:48 -0700

    Reduce test code and include test run images

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -105,15 +105,6 @@ def draw_boxes(image, boxes, classes, run_time, light_debug_index):
     plt.figure(figsize=(12, 8))
     plt.imshow(image)
     plt.savefig(run_time + "/boxes_image" + str(light_debug_index) + ".jpg")
-
-def vanilla_conv_block(x, kernel_size, output_channels):
-    """
-    Vanilla Conv -> Batch Norm -> ReLU
-    """
-    x = tf.layers.conv2d(
-        x, output_channels, kernel_size, (2, 2), padding='SAME')
-    x = tf.layers.batch_normalization(x)
-    return tf.nn.relu(x)
 # END TEST CODE
 
 class TLClassifier(object):
@@ -133,39 +124,13 @@ class TLClassifier(object):
             # input
             x = tf.constant(np.random.randn(1, IMG_HEIGHT, IMG_WIDTH, INPUT_CHANNELS), dtype=tf.float32)
 
-            with tf.variable_scope('vanilla'):
-                vanilla_conv = vanilla_conv_block(x, KERNEL_SIZE, OUTPUT_CHANNELS)
             with tf.variable_scope('mobile'):
                 mobilenet_conv = mobilenet_conv_block(x, KERNEL_SIZE, OUTPUT_CHANNELS)
 
-            vanilla_params = [
-                (v.name, np.prod(v.get_shape().as_list()))
-                for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'vanilla')
-            ]
             mobile_params = [
                 (v.name, np.prod(v.get_shape().as_list()))
                 for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'mobile')
             ]
-
-            print("VANILLA CONV BLOCK")
-            total_vanilla_params = sum([p[1] for p in vanilla_params])
-            for p in vanilla_params:
-                print("Variable {0}: number of params = {1}".format(p[0], p[1]))
-            print("Total number of params =", total_vanilla_params)
-            print()
-
-            print("MOBILENET CONV BLOCK")
-            total_mobile_params = sum([p[1] for p in mobile_params])
-            for p in mobile_params:
-                print("Variable {0}: number of params = {1}".format(p[0], p[1]))
-            print("Total number of params =", total_mobile_params)
-            print()
-
-            print("{0:.3f}x parameter reduction".format(total_vanilla_params /
-                                                     total_mobile_params))
-
-        print("Number of colors/classes, one color per class")
-
         # END TEST CODE
 
         # Detection Initialization
@@ -233,9 +198,8 @@ class TLClassifier(object):
             if READ_TEST_IMAGE:
                 width, height = image.size
             else:
-                height, width, channels = image.shape
-
             # END TEST CODE
+                height, width, channels = image.shape
 
             box_coords = to_image_coords(boxes, height, width)
 

4a3aae274530a3952d44fb3271b74778091f0040
Albert Vo <albert.vo@gmail.com>
2018-03-13 14:24:57 -0700

    tl_detector not loading

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -119,10 +119,11 @@ def draw_boxes(image, boxes, classes, run_time, light_debug_index):
 class TLClassifier(object):
     def __init__(self):
 
+        print ('######################       BEFORE Loaded Model')
         self.model = load_model('AVO7_v1_model.h5')
         self.model._make_predict_function()
         self.graph = tf.get_default_graph()
-        print ('######################Loaded Model')
+        print ('######################       AFTER Loaded Model')
         self.DO_RED_ONCE = True
         self.DO_YELLOW_ONCE = True
         self.DO_GREEN_ONCE = True
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -250,15 +250,16 @@ class TLDetector(object):
                     if DEBUG_LEVEL >= 2:
                         rospy.logwarn("TL Detector car wp: {0:d} pos: {1:.3f},{2:.3f}, {3:d}".format(car_position_wp, self.pose.pose.position.x, self.pose.pose.position.y,light_wp))
 
-            if self.has_image:
+#            if self.has_image:
 #                print ('HAS IMAGE')
-                state = self.get_light_state(self.light)
-                print ('---------Car_WP, Light_WP, Light_State = ', car_position_wp, light_wp, state )
-            else:
-                print ('NOOOOOOOOOOOOOOOOOO IMAGE')
+#                state = self.get_light_state(self.light)
+#                print ('---------Car_WP, Light_WP, Light_State = ', car_position_wp, light_wp, state )
+#            else:
+#                print ('NOOOOOOOOOOOOOOOOOO IMAGE')
 
 
             state = self.get_light_state(self.light)
+            print ('---------Car_WP, Light_WP, Light_State = ', car_position_wp, light_wp, state )
             return light_wp, state
         else:
             return -1, TrafficLight.UNKNOWN
